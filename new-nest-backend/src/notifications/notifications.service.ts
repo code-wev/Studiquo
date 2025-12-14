a92e29332c81722991efe954aeea2b0ee248563e
JoySarkarBD <developer.joysarkar@gmail.com>
2025-12-14 12:12:35 +0600

    refactor: remove node-cron dependency and related scheduling logic from NotificationsService

--- new-nest-backend/src/notifications/notifications.service.ts
+++ new-nest-backend/src/notifications/notifications.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import * as cron from 'node-cron';
 import { getUserSub } from '../common/helpers';
 import { Notification } from '../models/notification.model';
 import { CreateNotificationDto } from './dto/notification.dto';
@@ -11,13 +10,7 @@ export class NotificationsService {
   constructor(
     @InjectModel(Notification.name)
     private notificationModel: Model<Notification>,
-  ) {
-    // Example: send email notifications every hour (stub)
-    cron.schedule('0 * * * *', () => {
-      // Mailing logic stub
-      // console.log('Send scheduled email notifications');
-    });
-  }
+  ) {}
 
   async getMyNotifications(req: { user: any }) {
     return this.notificationModel.find({ user: getUserSub(req) });
